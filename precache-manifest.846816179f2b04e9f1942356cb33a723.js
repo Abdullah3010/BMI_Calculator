81d2feaad7ad15d6349668c34e63f945f38b0d26
Abdullah Mohamed <61066251+Abdullah3010@users.noreply.github.com>
2022-08-14 21:04:10 +0200

    Updates

---
+++ precache-manifest.846816179f2b04e9f1942356cb33a723.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d753fc6f5dda70f7783d",
+    "url": "/BMI_Calculator/static/css/main.42ea3ef8.chunk.css"
+  },
+  {
+    "revision": "d753fc6f5dda70f7783d",
+    "url": "/BMI_Calculator/static/js/main.d753fc6f.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/BMI_Calculator/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "90ce6ac22e87860e5869",
+    "url": "/BMI_Calculator/static/css/2.89968de3.chunk.css"
+  },
+  {
+    "revision": "90ce6ac22e87860e5869",
+    "url": "/BMI_Calculator/static/js/2.90ce6ac2.chunk.js"
+  },
+  {
+    "revision": "e816316c19b3af19863f",
+    "url": "/BMI_Calculator/static/js/runtime~main.e816316c.js"
+  },
+  {
+    "revision": "388bdf58f2819177f96394e65c87e2d0",
+    "url": "/BMI_Calculator/index.html"
+  }
+];
